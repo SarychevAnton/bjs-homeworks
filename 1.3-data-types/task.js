569f4a6fd8a0ca99aba80d7ca61b0e6db868653d
Anton Sarychev <anton.srychev.62@yandex.ru>
2021-03-02 01:26:01 +0300

    HW to wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,14 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    'use strict';
+    let creditAmount = amount - contribution;//сумма кредита
+    let today = new Date();
+    let creditTerm = Math.abs(date.getMonth()-today.getMonth() + (12*(date.getFullYear() - today.getFullYear())));//срок кредита
+    let monthContribution = creditAmount * ((percent/12/100) + (percent/12/100)/(((1 + (percent/12/100))**creditTerm)-1));//ежемесячный платеж
+    let totalAmount = monthContribution * creditTerm;
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    name ? greeting = `Привет, мир! Меня зовут ${name}.` : greeting = "Привет, мир! Меня зовут Аноним.";
+    return greeting;
 }
